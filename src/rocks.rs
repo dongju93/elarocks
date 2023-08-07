9c727635a57d5c32b0bc92945e00f3c49f864039
dong-ju <spdlqj011@gmail.com>
2023-08-08 01:51:26 +0900

    make rocksdb

---
+++ src/rocks.rs
@@ -0,0 +1,45 @@
+use rocksdb::{DB, WriteBatch, WriteOptions};
+use std::error::Error;
+use csv::ReaderBuilder;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Open or create a RocksDB database
+    let db = DB::open_default("/Users/dong-ju/Documents/My_code/elarocks/db")?;
+
+    // Read CSV data with column names
+    let csv_path = "/Users/dong-ju/Documents/My_code/elarocks/file/event1_processcreate.csv";
+    let mut rdr = ReaderBuilder::new().has_headers(true).from_path(csv_path)?;
+
+    // Start a write batch for bulk writes
+    let mut write_batch = WriteBatch::default();
+    let write_options = WriteOptions::default();
+
+    // Read the headers (column names) from the CSV file
+    let headers = rdr.headers()?.clone();
+
+    // Iterate over CSV rows and store in RocksDB
+    for result in rdr.records() {
+        let record = result?;
+        if let Some(timestamp) = record.get(0) {
+            // Convert the timestamp to a string (as a base key)
+            let base_key = timestamp;
+
+            // Iterate over the columns and store in the write batch
+            for (i, value) in record.iter().enumerate() {
+                if i != 0 {
+                    // Get the column name from the headers
+                    let column = &headers[i];
+
+                    let key = format!("{}_{}", base_key, column);
+                    write_batch.put(key.as_bytes(), value.as_bytes());
+                }
+            }
+        }
+    }
+
+    // Commit the write batch to perform bulk writes
+    db.write_opt(write_batch, &write_options)?;
+
+    // Close the database
+    Ok(())
+}
---
+++ src/select.rs
@@ -0,0 +1,39 @@
+use rocksdb::{DB, Options, DBIterator, IteratorMode};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Open the RocksDB database
+    let db = DB::open_default("/Users/dong-ju/Documents/My_code/elarocks/db")?;
+
+    // Example: Select data for a specific timestamp (wildcard match)
+    let partial_timestamp = "2023-08-07 12:05:02.936";
+    let base_key = partial_timestamp.as_bytes();
+
+    // Create an iterator for the database starting from the specified partial timestamp
+    let iter = db.iterator(IteratorMode::From(base_key, rocksdb::Direction::Forward));
+
+    // Iterate over the data entries with keys matching the wildcard
+    for item in iter {
+        match item {
+            Ok((key, value)) => {
+                // Convert the key to a string
+                let key_str = String::from_utf8(key.to_vec())?;
+
+                // Check if the key includes the desired partial timestamp
+                if key_str.contains(partial_timestamp) {
+                    let value_str = String::from_utf8(value.to_vec())?;
+                    println!("Key: {}, Value: {}", key_str, value_str);
+                } else {
+                    // If the key does not contain the desired partial timestamp, break the loop
+                    break;
+                }
+            }
+            Err(e) => {
+                eprintln!("Error while iterating: {:?}", e);
+                // Handle the error as needed
+            }
+        }
+    }
+
+    Ok(())
+}
