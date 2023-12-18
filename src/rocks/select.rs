27af410b1226a2b7bd181e6bf600bc3f584836a1
dong-ju <spdlqj011@gmail.com>
2023-12-19 05:40:58 +0900

    iteration with pagination

--- src/rocks/select.rs
+++ src/rocks/select.rs
@@ -1,35 +1,215 @@
-use rocksdb::{IteratorMode, DB};
+use chrono::{NaiveDateTime, TimeZone, Utc};
+use rocksdb::{Direction, IteratorMode, DB};
+use serde_json::{Map, Value};
+use std::env;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut total_count = 0;
+    let mut written_count = 0;
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        return Err("Please provide start_key and end_key as arguments".into());
+    }
+
+    let start_key = args[1].as_bytes();
+    let end_key = args[2].as_bytes();
+    let direction = args.get(3).map_or(Direction::Forward, |arg| {
+        if arg == "last" {
+            Direction::Reverse
+        } else {
+            Direction::Forward
+        }
+    });
+    let input_max_records = args
+        .get(4)
+        .and_then(|s| s.parse::<usize>().ok())
+        .unwrap_or(usize::MAX);
+    let cursor = args.get(5).and_then(|s| s.parse::<i64>().ok());
+    let image_contain = args.get(6).map(String::as_str); // Optional argument
+    let pid_match = args.get(7).and_then(|s| s.parse::<u32>().ok());
+
+    let iter_mode = match direction {
+        Direction::Forward => IteratorMode::From(start_key, Direction::Forward),
+        Direction::Reverse => IteratorMode::From(end_key, Direction::Reverse),
+    };
+
     let db = DB::open_default("/Users/dong-ju/Documents/My_code/elarocks/db")?;
+    let mut iter = db.iterator(iter_mode);
 
-    let partial_timestamp = "Network connection detected_2023-09-07 01:58:50.13600001";
-    let base_key = partial_timestamp.as_bytes();
-
-    // Create an iterator for the database starting from the specified partial timestamp
-    let iter = db.iterator(IteratorMode::From(base_key, rocksdb::Direction::Forward));
-
-    // Iterate over the data entries with keys
-    for item in iter {
-        match item {
-            Ok((key, value)) => {
-                // Convert the key to a string
-                let key_str = String::from_utf8(key.to_vec())?;
-
-                // Check if the key includes the desired partial timestamp
-                if key_str.contains(partial_timestamp) {
-                    let value_str = String::from_utf8(value.to_vec())?;
-                    println!("Key: {}, Value: {}", key_str, value_str);
-                } else {
-                    break;
-                }
+    let count_iter_mode = IteratorMode::From(start_key, Direction::Forward);
+    let mut count_iter = db.iterator(count_iter_mode);
+    for item in &mut count_iter {
+        if let Ok((key, value)) = item {
+            if key.as_ref() > end_key {
+                break;
             }
-            Err(e) => {
-                eprintln!("Error while iterating: {:?}", e);
+            let value_str = String::from_utf8(value.to_vec())?;
+            let json: Map<String, Value> = serde_json::from_str(&value_str)?;
+            if image_contain.map_or(true, |img| {
+                json.get("image")
+                    .and_then(|v| v.as_str())
+                    .map_or(false, |image| image.contains(img))
+            }) && pid_match.map_or(true, |pid| {
+                json.get("process_id")
+                    .and_then(|v| v.as_u64())
+                    .map_or(false, |id| id as u32 == pid)
+            }) {
+                total_count += 1;
             }
         }
     }
 
+    if let Some(cursor_time) = cursor {
+        while let Some(result) = iter.next() {
+            let (key, _) = result?;
+
+            let key_time = key_to_epoch(&String::from_utf8(key.to_vec())?)?;
+            let condition = match direction {
+                Direction::Forward => key_time >= cursor_time,
+                Direction::Reverse => key_time <= cursor_time,
+            };
+
+            if condition {
+                break;
+            }
+        }
+    }
+
+    let mut has_next_page = false;
+    let mut has_previous_page = false;
+    let mut last_key = None;
+    let mut first_key = None;
+    let mut is_cursor_condition_met = cursor.is_none(); // true if cursor is not provided
+
+    print!("[");
+    for item in &mut iter {
+        if let Ok((key, value)) = item {
+            // Check if the key is within the specified range
+            match direction {
+                Direction::Reverse => {
+                    if key.as_ref() < start_key {
+                        break;
+                    }
+                }
+                Direction::Forward => {
+                    if key.as_ref() > end_key {
+                        break;
+                    }
+                }
+            }
+
+            // Process the value
+            let value_str = String::from_utf8(value.to_vec())?;
+            let json: Map<String, Value> = serde_json::from_str(&value_str)?;
+
+            // Check for image and pid match
+            let image_match = image_contain.map_or(true, |img| {
+                json.get("image")
+                    .and_then(|v| v.as_str())
+                    .map_or(false, |image| image.contains(img))
+            });
+
+            let pid_match = pid_match.map_or(true, |pid| {
+                json.get("process_id")
+                    .and_then(|v| v.as_u64())
+                    .map_or(false, |id| id as u32 == pid)
+            });
+
+            if image_match && pid_match {
+                // Check if the cursor condition is met
+                if !is_cursor_condition_met {
+                    let key_time = key_to_epoch(&String::from_utf8(key.to_vec())?)?;
+                    is_cursor_condition_met = match direction {
+                        Direction::Forward => key_time >= cursor.unwrap_or(0),
+                        Direction::Reverse => key_time <= cursor.unwrap_or(i64::MAX),
+                    };
+                }
+
+                // Check if the record is within the pagination window
+                if is_cursor_condition_met && written_count < input_max_records {
+                    let epoch_time = key_to_epoch(&String::from_utf8(key.to_vec())?)?;
+                    print!(
+                        "{{\"cursor\": \"{}\", \"node\": {}}},",
+                        epoch_time, value_str
+                    );
+                    written_count += 1;
+
+                    if first_key.is_none() {
+                        first_key = Some(key.clone());
+                    }
+                    last_key = Some(key.clone());
+                }
+            }
+        }
+    }
+
+    if let Some(last_key) = last_key {
+        has_previous_page = if cursor.is_some() {
+            match direction {
+                Direction::Forward => has_more_records(
+                    &db,
+                    start_key,
+                    first_key.as_deref().unwrap_or(start_key),
+                    Direction::Forward,
+                ),
+                Direction::Reverse => has_more_records(
+                    &db,
+                    end_key,
+                    first_key.as_deref().unwrap_or(end_key),
+                    Direction::Reverse,
+                ),
+            }
+        } else {
+            false
+        };
+
+        has_next_page = match direction {
+            Direction::Forward => {
+                has_more_records(&db, last_key.as_ref(), end_key, Direction::Forward)
+            }
+            Direction::Reverse => {
+                has_more_records(&db, last_key.as_ref(), start_key, Direction::Reverse)
+            }
+        };
+    }
+
+    println!(
+        "{{\"total_count\": {}, \"written_count\": {}, \"has_next_page\": {}, \"has_previous_page\": {}}}]",
+        total_count, written_count, has_next_page, has_previous_page
+    );
+
     Ok(())
+}
+
+fn key_to_epoch(key: &str) -> Result<i64, Box<dyn Error>> {
+    let parts: Vec<&str> = key.split('_').collect();
+    let timestamp_str = parts.get(1).ok_or("Timestamp not found in key")?;
+
+    let naive_dt = NaiveDateTime::parse_from_str(timestamp_str, "%Y-%m-%d %H:%M:%S%.f")?;
+
+    let datetime = Utc.from_utc_datetime(&naive_dt);
+
+    match datetime.timestamp_nanos_opt() {
+        Some(nanos) => Ok(nanos),
+        None => Err("Failed to convert to nanoseconds".into()),
+    }
+}
+
+fn has_more_records(db: &DB, key: &[u8], boundary_key: &[u8], direction: Direction) -> bool {
+    let iter_mode = IteratorMode::From(key, direction);
+    let mut iter = db.iterator(iter_mode);
+    // println!("{:?}", boundary_key);
+
+    match iter.next() {
+        Some(Ok((k, _))) => {
+            let k_slice = k.as_ref(); // Convert Box<[u8]> to &[u8]
+            match direction {
+                Direction::Forward => k_slice < boundary_key,
+                Direction::Reverse => k_slice > boundary_key,
+            }
+        }
+        _ => false,
+    }
 }
